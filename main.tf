395c2c775883ac1521bbda74519b33d66a20a0c9
Alex Koch <alex549us3@gmail.com>
2018-05-20 01:39:58 +0000

    adds variables, still in same file

--- main.tf
+++ main.tf
@@ -1,15 +1,35 @@
+variable "image" {
+  description = "image for container"
+  default = "ghost:latest"
+}
+
+variable "container_name" {
+  description = "name for container"
+  default = "blog"
+}
+
+variable "int_port" {
+  description = "internal port for container"
+  default = "2368"
+}
+
+variable "ext_port" {
+  description = "external port for container"
+  default = "80"
+}
+
 # Download the latest Ghost image
 resource "docker_image" "image_id" {
-  name = "ghost:latest"
+  name = "${var.image}"
 }
 
 # Start the Container
 resource "docker_container" "container_id" {
-  name = "blog"
+  name = "${var.container_name}"
   image = "${docker_image.image_id.latest}"
   ports {
-    internal = "2368"
-    external = "80"
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
   }
 }
 
